cd1259e19195041cc649b956004ab475294fef83
Wali Seddiqi <waliseddiqi4@gmail.com>
2021-01-29 23:11:28 +0300

    enums added

--- lib/core/enums.dart
+++ lib/core/enums.dart
@@ -1,2 +1,4 @@
 enum ConnectivityStatus { WiFi, Cellular, Offline }
 enum Auth{Authicated,NotAuthicated}
+//change the local address as IPV4 local address
+String localaddress="http://192.168.137.1:5000";
