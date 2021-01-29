68ea66a0101e682528de9be16ea9d20d21aaaca8
Wali Seddiqi <waliseddiqi4@gmail.com>
2021-01-29 23:37:09 +0300

    Revert "ddd"
    This reverts commit adc701fdc4c87103b468d2e20d72a36ef790f578.

--- lib/models/api.dart
+++ lib/models/api.dart
@@ -1,14 +1,11 @@
 import 'dart:convert';
 import 'dart:io';
 import 'package:http/http.dart'as http;
-import 'package:intership/core/enums.dart';
 abstract class BaseAPI{
 
 
+Future<String> sendImageAndInfo(File file,int numberPlate);
 
-Future<http.Response> sendImage(File file,String numberPlate,int random);
-Future<http.Response> sendInfo(String numberPlate,String address,String imageurl);
-Future<http.Response> sendlocation(String address);
 
 
 
@@ -17,53 +14,21 @@ Future<http.Response> sendlocation(String address);
 class API implements BaseAPI{
   final Map<String,String> headers = {'Accept': 'application/json',"Content-type": "application/json"};
   @override
-  Future<http.Response>  sendImage(File file,String numberPlate,int random)async{
+  Future<String>  sendImageAndInfo(File file,int numberPlate)async{
     var response;
     try {
      var data=file.readAsBytesSync();
      String base64=base64Encode(data);
      String format=file.path.split("/").last.split(".").last;
      
-      var body={"image":base64,"plateno":numberPlate.toString(),"format":format,"random":random};
-      response=http.post(localaddress+"/image",body:jsonEncode(body),headers: headers);
+      var body={"image":base64,"name":numberPlate.toString(),"format":format};
+      response=http.post("http://192.168.137.1:5000/info",body:jsonEncode(body),headers: headers);
 
     } catch (e) {
       print(e);
     }
- 
-    return response;
-  }
-
-  
-    @override
-    Future<http.Response> sendInfo(String numberPlate, String address, String imageurl) {
-        var response;
-    try {
-    
-     
-      var body={"PlateNo":numberPlate,"Imageurl":imageurl,"Address":address};
-      response=http.post(localaddress+"/info",body:jsonEncode(body),headers: headers);
-
-    } catch (e) {
-      print(e);
-    }
-   return response;
-
-  }
-
-  @override
-  Future<http.Response> sendlocation(String address) {
-        var response;
-    try {
-    
-     
-      var body={"location":address};
-      response=http.post(localaddress+"/localtion",body:jsonEncode(body),headers: headers);
-
-    } catch (e) {
-      print(e);
-    }
-   return response;
+   print(response);
+    return "dd";
   }
 
 }
